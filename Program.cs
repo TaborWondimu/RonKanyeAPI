b7a02fda6c325444ce0783ba86bd68f258fbcee0
Tabor Wondimu <csmajortabor@gmail.com>
2022-03-29 11:32:45 -0700

    Finished the Excersice

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+namespace RonKanyeAPI
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            for (int i = 0; i < 5; i++)
+            {
+                var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+                var KanyeURL = "https://api.kanye.rest";
+
+                var ronResponse = client.GetStringAsync(ronURL).Result;
+                var kanyeResponse = client.GetStringAsync(KanyeURL).Result;
+
+                var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+                var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+                Console.WriteLine($"Kanye talking: {kanyeQuote} ");
+                Console.WriteLine($"Ron talking: { ronQuote} ");    
+
+
+            }
+
+
+        }
+    }
+}
